7a543b21124197834836367cb36900e80b199319
Aaron Worsham <aaronworsham@gmail.com>
2011-09-22 08:06:37 -0700

    more work on the dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,9 +2,11 @@ class DashboardController < ApplicationController
   before_filter :login_required
   
   def show
-    @portfolio = current_user.portfolios.first
-    @holdings = @portfolio.try('holdings') || []
+    @portfolios = current_user.portfolios
+    @holdings = current_user.holdings
     @messages = MessageEveryone.head
     @messages_everyone_count = MessageEveryone.head_count
+    @friends = current_user.friends
+    @following = current_user.following
   end
 end
