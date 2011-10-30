2bc0c86501afc259d1967ba448ce4f046ba0690c
Aaron Worsham <aaronworsham@gmail.com>
2011-10-30 13:56:02 -0400

    we now have a trust system

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,49 +24,74 @@ class User < ActiveRecord::Base
   
 
 
-  def follow!(user)
+  def trust!(user, level)
     $redis.multi do
-      $redis.sadd(self.redis_key(:following), user.id)
-      $redis.sadd(user.redis_key(:followers), self.id)
+      $redis.sadd(self.redis_key(:trusting), user.id)
+      $redis.sadd(user.redis_key(:trusted_by), self.id)
     end
+    self.set_clearance(user, level)
   end
   # unfollow a user
-  def unfollow!(user)
+  def untrust!(user)
     $redis.multi do
-      $redis.srem(self.redis_key(:following), user.id)
-      $redis.srem(user.redis_key(:followers), self.id)
+      $redis.srem(self.redis_key(:trusting), user.id)
+      $redis.srem(user.redis_key(:trusted_by), self.id)
     end
   end
   # users that self follows
-  def followers
-    user_ids = $redis.smembers(self.redis_key(:followers))
+  def trusting
+    user_ids = $redis.smembers(self.redis_key(:trusting))
     User.where(:id => user_ids)
   end
   # users that follow self
-  def following
-    user_ids = $redis.smembers(self.redis_key(:following))
+  def trusted_by
+    user_ids = $redis.smembers(self.redis_key(:trusted_by))
     User.where(:id => user_ids)
   end
   # users who follow and are being followed by self
   def friends
-    user_ids = $redis.sinter(self.redis_key(:following), self.redis_key(:followers))
+    user_ids = $redis.sinter(self.redis_key(:trusting), self.redis_key(:trusted_by))
     User.where(:id => user_ids)
   end
   # does the user follow self
-  def followed_by?(user)
-    $redis.sismember(self.redis_key(:followers), user.id)
+  def trusted_by?(user)
+    $redis.sismember(self.redis_key(:trusted_by), user.id)
   end
   # does self follow user
-  def following?(user)
-    $redis.sismember(self.redis_key(:following), user.id)
+  def trusting?(user)
+    $redis.sismember(self.redis_key(:trusting), user.id)
   end
   # number of followers
-  def followers_count
-    $redis.scard(self.redis_key(:followers))
+  def trusted_by_count
+    $redis.scard(self.redis_key(:trusted_by))
   end
   # number of users being followed
-  def following_count
-    $redis.scard(self.redis_key(:following))
+  def trusting_count
+    $redis.scard(self.redis_key(:trusting))
+  end
+  def has_clearance?(user, level)
+    return false unless self.trusting?(user)
+    return false if level == 0
+    (level..10).each do |x|
+      return true if $redis.sismember(self.redis_key("trust:level:#{x}"), user.id)
+    end
+    return false
+  end
+  def set_clearance(user, level)
+    self.clear_user_clearance(user, level)
+    $redis.sadd(self.redis_key("trust:level:#{level}"),user.id)
+  end
+  def clear_user_clearance(user, level)
+    (0..10).each do |x|
+      $redis.srem(self.redis_key("trust:level:#{level}"), user.id)
+    end
+  end
+  def trust_level(user)
+    return false unless self.trusting?(user)
+    (0..10).each do |x|
+      return x if $redis.sismember(self.redis_key("trust:level:#{x}"), user.id)
+    end
+    return 0
   end
   # helper method to generate redis keys
   def redis_key(str)
