643e9fe36bc94273a72d671de6cd722d8710797c
Sara Gibbons <sarag@vacationview.com>
2012-01-19 18:56:31 -0500

    fill in add stock spec

--- spec/requests/portfolio/add_stock_spec.rb
+++ spec/requests/portfolio/add_stock_spec.rb
@@ -11,15 +11,32 @@ feature "add stock to portfolio" do
     within('#portfolios-hotlist') do
       page.should have_content(@portfolio.name)
     end
+    within('#warmlist-portfolios') do
+      page.should have_css('li.add-holding.add-stock')
+    end
   end
-  
-  scenario 'add form has correct fields' do
-  end
-  
+
   scenario 'create new stock' do
+    find('li.add-holding.add-stock').click
+    within('#new_stock_holding') do
+      fill_in 'stock_holding_ticker_name', :with => 'Oscar Meyer'
+      fill_in 'stock_holding_ticker_symbol', :with => 'OSC'
+      fill_in 'stock_holding_starting_shares', :with => '10'
+      fill_in 'stock_holding_starting_price', :with => '$7.50'
+      select '2010', :from => 'stock_holding_purchased_at_1i'
+      select 'October', :from => 'stock_holding_purchased_at_2i'
+      select '11', :from => 'stock_holding_purchased_at_3i'
+      click_on('Create')
+    end
+    @portfolio.stock_holdings.size.should == 1
   end
   
   scenario 'validates for required information' do
+    find('li.add-holding.add-stock').click
+    within('#new_stock_holding') do
+      click_on('Create')
+    end
+    @portfolio.stock_holdings.size.should == 0
   end
   
 end
