292b23c98c73f814a7e1e25a9675e7a9f4cb39af
Scott Goci <sgoci@sazboom.com>
2012-06-04 10:53:04 -0400

    Adding admin interface using ActiveAdmin, along with a dashboard view for Users

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ CommonBeans::Application.routes.draw do
 
 
 
+  ActiveAdmin.routes(self)
+
+  devise_for :admin_users, ActiveAdmin::Devise.config
+
   resources :trackers
   resources :indices
   resources :fund_tickers
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,49 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120527195118) do
+ActiveRecord::Schema.define(:version => 20120604144142) do
+
+  create_table "active_admin_comments", :force => true do |t|
+    t.string   "resource_id",   :null => false
+    t.string   "resource_type", :null => false
+    t.integer  "author_id"
+    t.string   "author_type"
+    t.text     "body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "namespace"
+  end
+
+  add_index "active_admin_comments", ["author_type", "author_id"], :name => "index_active_admin_comments_on_author_type_and_author_id"
+  add_index "active_admin_comments", ["namespace"], :name => "index_active_admin_comments_on_namespace"
+  add_index "active_admin_comments", ["resource_type", "resource_id"], :name => "index_admin_notes_on_resource_type_and_resource_id"
+
+  create_table "admin_users", :force => true do |t|
+    t.string   "email",                  :default => "", :null => false
+    t.string   "encrypted_password",     :default => "", :null => false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          :default => 0
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "admin_users", ["email"], :name => "index_admin_users_on_email", :unique => true
+  add_index "admin_users", ["reset_password_token"], :name => "index_admin_users_on_reset_password_token", :unique => true
+
+  create_table "advisors", :force => true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.string   "keycode"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "bond_events", :force => true do |t|
     t.integer  "bond_ticker_id"
@@ -401,6 +443,15 @@ ActiveRecord::Schema.define(:version => 20120527195118) do
     t.string   "current_state"
     t.boolean  "lead",                                            :default => false
     t.string   "name"
+  end
+
+  create_table "identities", :force => true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.string   "keycode"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "index_eods", :force => true do |t|
