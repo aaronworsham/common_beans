4d6766bb316d5b89a596c47b643d0c484054f8f5
Aaron Worsham <aaronworsham@gmail.com>
2012-07-09 20:09:49 -0400

    handle auth failure better

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -24,4 +24,10 @@ class SessionsController < ApplicationController
     session[:user_id] = nil  
     redirect_to root_url, :notice => "Signed out!"
   end
+
+  def failure
+    session[:user_id] = nil  
+    cookies[:cb_notify] = "We were unable to authorized you using your Social Network"
+    redirect_to root_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -64,6 +64,7 @@ CommonBeans::Application.routes.draw do
 
   match "/auth/:provider/callback" => "sessions#create"
   match "/signout" => "sessions#destroy", :as => :signout
+  match "/auth/failure" => "sessions#failure"
   root :to => 'pages#index'
 
   # The priority is based upon order of creation:
